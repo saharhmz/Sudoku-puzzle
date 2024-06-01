1446e0825d8add3c5661688b11213e0fa6c6d8b2
sahar hooshmand <94601788+saharhmz@users.noreply.github.com>
2024-06-02 01:34:54 +0330

    Update Genetic.java

--- sudoku/src/main/java/model/Genetic.java
+++ sudoku/src/main/java/model/Genetic.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.util.Random;
+
 public class Genetic {
+
+    private final int populationSize =360;
+    private final double mutationRate = 0.05;
+    private final int[][] initialIndividual;
+    private int generation;
+    private Random random = new Random();
+
+    public Genetic(int[][] initialIndividual) {
+        this.initialIndividual = initialIndividual;
+    }
+
+    private void setGeneration(int generation){
+        this.generation=generation;
+    }
+    public int getGeneration(){
+        return this.generation;
+    }
+
+    
 }
