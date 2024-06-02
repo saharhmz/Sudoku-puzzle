834c2ce38dc7dec345be41168edd229501f24b45
shirinrohanian <94758700+shirinrohanian@users.noreply.github.com>
2024-06-02 23:06:31 +0330

    Update Individual.java

--- sudoku/src/main/java/model/Individual.java
+++ sudoku/src/main/java/model/Individual.java
@@ -1,4 +1,43 @@
 package model;
 
-public class Individual {
+
+public class Individual implements Comparable<Individual> {
+    private int[][] individual;
+    private int fitness;
+
+
+    public Individual(int[][] individual, int fitness) {
+        this.individual = individual;
+        this.fitness = fitness;
+    }
+    public Individual(){}
+
+    public int[][] getIndividual() {
+        return individual;
+    }
+
+    public void setPopulation(int[][] individual) {
+        this.individual = individual;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    @Override
+    public int compareTo(Individual other) {
+        if (this.fitness < other.fitness) {
+            return -1;
+        }
+        else if (this.fitness > other.fitness) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
